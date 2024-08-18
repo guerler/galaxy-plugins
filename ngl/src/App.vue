9f309d65a3eca344b45765c60809dfb4d4f1295b
guerler <aysam.guerler@gmail.com>
2024-08-18 10:12:16 +0300

    Add ngl plugin

---
+++ ngl/src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ViewPort } from "galaxy-charts";
+import Plugin from "@/Plugin.vue";
+const config = {
+    dataset_url: "1ACB.pdb",
+    settings: {
+        setting_text: "my test text",
+        setting_boolean: true,
+    },
+};
+</script>
+
+<template>
+    <ViewPort :config="config" xml="galaxy-charts.xml">
+        <template #default="{ datasetId, datasetUrl, root, settings, specs, tracks }">
+            <Plugin
+                :dataset-id="datasetId"
+                :dataset-url="datasetUrl"
+                :root="root"
+                :settings="settings"
+                :specs="specs"
+                :tracks="tracks" />
+        </template>
+    </ViewPort>
+</template>
